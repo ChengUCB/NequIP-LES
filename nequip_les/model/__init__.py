4ec38f60a430b82f5f1abcb7917a632a8c3298bb
cw-tan <chuinwei.tan@gmail.com>
2025-11-20 15:13:01 -0500

    update and lint LES model builders

--- nequip_les/model/__init__.py
+++ nequip_les/model/__init__.py
@@ -1,5 +1,5 @@
 # This file is a part of the `nequip-les` package. Please see LICENSE and README at the root for information on using it.
-from .les_model import LESModel, LESEnergyModel
+from .les_model import LESModel
 from .les_output import Add_LES_to_NequIP_model, Add_LES_to_Allegro_model
 
-__all__ = ["LESModel", "LESEnergyModel", "Add_LES_to_NequIP_model", "Add_LES_to_Allegro_model"]
+__all__ = ["LESModel", "Add_LES_to_NequIP_model", "Add_LES_to_Allegro_model"]
