79967ed27b7367c882511048a920f8578b02d4d9
cw-tan <chuinwei.tan@gmail.com>
2025-11-26 14:50:15 -0500

    extern les for `nequip-package`

--- nequip_les/__init__.py
+++ nequip_les/__init__.py
@@ -1,5 +1,6 @@
 # This file is a part of the `nequip-les` package. Please see LICENSE and README at the root for information on using it.
 from ._version import __version__
 from . import _keys
+from . import _extern
 
-__all__ = ["__version__"]
+__all__ = ["__version__", "_extern"]
