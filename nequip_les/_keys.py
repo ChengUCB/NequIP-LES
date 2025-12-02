7d6bbc727443a979a4d1d4e38db75b7766531c39
cw-tan <chuinwei.tan@gmail.com>
2025-12-01 20:06:44 -0500

    run `ruff` on `_keys.py`

--- nequip_les/_keys.py
+++ nequip_les/_keys.py
@@ -2,7 +2,7 @@
 from typing import Final
 from nequip.data._key_registry import register_fields
 
-#key definitions for nequip_les
+# key definitions for nequip_les
 LATENT_CHARGE_KEY: Final[str] = "LES_q"
 LR_ENERGY_KEY: Final[str] = "lr_energy"
 SR_ENERGY_KEY: Final[str] = "sr_energy"
@@ -10,22 +10,11 @@ BEC_KEY: Final[str] = "LES_BEC"
 EDGE_LATENT_CHARGE_KEY: Final[str] = "edge_LES_q"
 
 
-#key registry for nequip_les
-#check nequip.data._key_registry for more information
+# key registry for nequip_les
+# check nequip.data._key_registry for more information
 register_fields(
-    node_fields=[
-        LATENT_CHARGE_KEY, 
-        BEC_KEY
-    ],
-    graph_fields=[
-        LR_ENERGY_KEY, 
-
-        SR_ENERGY_KEY
-    ],
-    edge_fields=[
-        EDGE_LATENT_CHARGE_KEY
-    ],
-    cartesian_tensor_fields={
-        BEC_KEY: "ij" 
-    }
+    node_fields=[LATENT_CHARGE_KEY, BEC_KEY],
+    graph_fields=[LR_ENERGY_KEY, SR_ENERGY_KEY],
+    edge_fields=[EDGE_LATENT_CHARGE_KEY],
+    cartesian_tensor_fields={BEC_KEY: "ij"},
 )
