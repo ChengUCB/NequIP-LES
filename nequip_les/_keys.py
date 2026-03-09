cf71cdf02bb99c58cbe6fe89f6ae6f1e2718a0b4
djkim-kr <dongjin.kim@berkeley.edu>
2026-03-08 21:51:44 -0700

    added keys, and module for vector readout

--- nequip_les/_keys.py
+++ nequip_les/_keys.py
@@ -8,12 +8,18 @@ LR_ENERGY_KEY: Final[str] = "lr_energy"
 SR_ENERGY_KEY: Final[str] = "sr_energy"
 BEC_KEY: Final[str] = "LES_BEC"
 EDGE_LATENT_CHARGE_KEY: Final[str] = "edge_LES_q"
+# dipole LES
+LATENT_DIPOLE_KEY: Final[str] = "LES_u"
+LATENT_POLARIZABILITY_KEY: Final[str] = "LES_alpha"
+LATENT_CHEMICAL_SOFTNESS_KEY: Final[str] = "LES_kappa"
 
 
 # key registry for nequip_les
 # check nequip.data._key_registry for more information
 register_fields(
-    node_fields=[LATENT_CHARGE_KEY, BEC_KEY],
+    node_fields=[LATENT_CHARGE_KEY, BEC_KEY,
+                 LATENT_DIPOLE_KEY, LATENT_POLARIZABILITY_KEY, 
+                 LATENT_CHEMICAL_SOFTNESS_KEY],
     graph_fields=[LR_ENERGY_KEY, SR_ENERGY_KEY],
     edge_fields=[EDGE_LATENT_CHARGE_KEY],
     cartesian_tensor_fields={BEC_KEY: "ij"},
--- nequip_les/_version.py
+++ nequip_les/_version.py
@@ -3,4 +3,5 @@
 # See Python packaging guide
 # https://packaging.python.org/guides/single-sourcing-package-version/
 
-__version__ = "0.1.0"
+# __version__ = "0.1.0"
+__version__ = "0.2.0"
